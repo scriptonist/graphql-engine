020e8ed8f0855f017a8d73e17c64771556d06be5
scriptonist <aravindkp@hasura.io>
2020-01-30 17:13:09 +0530

    remove deadcode

--- cli/commands/seed.go
+++ cli/commands/seed.go
@@ -1,21 +1,12 @@
 package commands
 
 import (
-	"sync"
-
 	"github.com/hasura/graphql-engine/cli"
 	"github.com/spf13/cobra"
 )
 
 // NewSeedCmd will return the seed command
 func NewSeedCmd(ec *cli.ExecutionContext) *cobra.Command {
-	// v := viper.New()
-
-	/*
-		opts := &seedCmdOptions{
-			EC: ec,
-		}
-	*/
 	seedCmd := &cobra.Command{
 		Use:          "seed",
 		Short:        "work with seed data",
@@ -29,24 +20,4 @@ func NewSeedCmd(ec *cli.ExecutionContext) *cobra.Command {
 	)
 
 	return seedCmd
-}
-
-type seedCmdOptions struct {
-	EC *cli.ExecutionContext
-
-	APIPort     string
-	ConsolePort string
-	Address     string
-
-	DontOpenBrowser bool
-
-	WG *sync.WaitGroup
-
-	StaticDir string
-	Browser   string
-}
-
-func (o *seedCmdOptions) run() error {
-	// log := o.EC.Logger
-	return nil
 }
